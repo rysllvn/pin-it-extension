9fa5839bcb9ebe1ab4cd82743a6a0a579e19e329
Ryan Sullivan <mrsulliv@gmail.com>
2022-10-19 12:37:58 -0700

    move install page to own repo

--- src/background_scripts/background.ts
+++ src/background_scripts/background.ts
@@ -1,7 +1,20 @@
+browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+  if (temporary) return; // skip during development
+  switch (reason) {
+    case "install":
+      {
+        await browser.tabs.create({ url: 'https://rysllvn.github.io/pin-it-info-page/' });
+      }
+      break;
+    // see below
+  }
+});
+
 browser.browserAction.onClicked.addListener(() => {
   browser.tabs.query({currentWindow: true, active: true})
     .then(tabs => {
       if (typeof tabs[0] !== 'undefined' && typeof tabs[0].id === 'number') {
+        browser.tabs.create({ url: 'https://rysllvn.github.io/pin-it-info-page/' });
         browser.tabs.sendMessage(tabs[0].id, 'toggle');
       }
     })
