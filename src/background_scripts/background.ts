24202edb917f0f70ff740622ac139400e228ed7d
Ryan Sullivan <mrsulliv@gmail.com>
2022-10-20 08:42:52 -0700

    fix bug opening page on icon click

--- src/background_scripts/background.ts
+++ src/background_scripts/background.ts
@@ -14,7 +14,6 @@ browser.browserAction.onClicked.addListener(() => {
   browser.tabs.query({currentWindow: true, active: true})
     .then(tabs => {
       if (typeof tabs[0] !== 'undefined' && typeof tabs[0].id === 'number') {
-        browser.tabs.create({ url: 'https://rysllvn.github.io/pin-it-info-page/' });
         browser.tabs.sendMessage(tabs[0].id, 'toggle');
       }
     })
